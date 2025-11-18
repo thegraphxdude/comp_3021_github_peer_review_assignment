07d85dfbfd44a3b3c8387df4bba1e4ab26538427
ppirogov-rrc <ppirogov@rrc.ca>
2025-11-17 19:13:37 -0600

    Added fixes to the python file.

--- assignment1_code_sample_1.py
+++ assignment1_code_sample_1.py
@@ -8,18 +8,32 @@ db_config = {
     'password': 'secret123'
 }
 
+
 def get_user_input():
     user_input = input('Enter your name: ')
     return user_input
 
+# Injection again, 
+
+# OWASP Category: Injection
+#Fix: avoid using os.system as it just pases what you write into the shell
 def send_email(to, subject, body):
     os.system(f'echo {body} | mail -s "{subject}" {to}')
 
+
+# Insecure because the url is not encrypted, uses http instead of https
+# No validation or timeout in urlopen
+
+# OWASP Category: Security Misconfiguration & Cryptographic Failures
+# Fix: Use https and add timeouts
 def get_data():
     url = 'http://insecure-api.com/get-data'
     data = urlopen(url).read().decode()
     return data
 
+# SQL Injection
+
+# Injection
 def save_to_db(data):
     query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
     connection = pymysql.connect(**db_config)
